2bde68e4c8e6601a58ebf600eee91e8e477cc5fd
David <mysteryzonmusic@gmail.com>
2025-05-31 16:27:57 -0400

    Create navi.js

---
+++ scripts/navi.js
@@ -0,0 +1,13 @@
+/**
+ * these ARE the navigation items for the sidebar
+ */
+
+const navItems = [
+  { title: "Home", icon: "fa-home", url: "page/home.html" },
+  { title: "Games", icon: "fa-gamepad-modern", url: "page/games.html" },
+  { title: "VM", icon: "fa-desktop", url: "page/vm-priv.html" },
+  { title: "Browser", icon: "fa-search", url: "/scram.html" },
+  { title: "Apps", icon: "fa-grid", url: "page/apps.html" },
+  { title: "Chat", icon: "fa-comments-alt", url: "page/chat.html" },
+  { title: "Movies", icon: "fa-clapperboard", url: "page/mov.html" },
+];
